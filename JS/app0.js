55bcbb94d420ce0c9316e58e0c19a08df3f4fbe2
Emanwhibi <whibieman@gmail.com>
2023-06-12 20:13:46 +0000

    continue on the Js file for the shopping page

--- JS/app0.js
+++ JS/app0.js
@@ -75,3 +75,70 @@ function handler30(e) {
     i30++;
   }
 };
+
+let saveValues40 = document.getElementById('form40');
+saveValues40.addEventListener('submit', handler40)
+function handler40(e) {
+  e.preventDefault();
+  if (i40 == 0) {
+    new bouquet("Birthday", "Purple Tulib Flower", 5, "Assets/shoppingImages/Shooping4.jpg", "Majestic purple irises evoke a sense of wonder and allure.",  );
+    console.log(bouquetArray);
+    storeIntoLocalStorage();
+    i40++;
+  }
+};
+let saveValues50 = document.getElementById('form50');
+saveValues50.addEventListener('submit', handler50)
+function handler50(e) {
+  e.preventDefault();
+  if (i50 == 0) {
+    new bouquet("Birthday", "Glowy pink Flower", 5, "Assets/shoppingImages/Shooping5.jpg", "Delicate pink , with their grace and charm.",  );
+    console.log(bouquetArray);
+    storeIntoLocalStorage();
+    i50++;
+  }
+};
+let saveValues60 = document.getElementById('form60');
+saveValues60.addEventListener('submit', handler60)
+function handler60(e) {
+  e.preventDefault();
+  if (i60 == 0) {
+    new bouquet("Birthday", "Yellow Juri Flower", 5, "Assets/shoppingImages/Shooping6.jpg", "Vibrant yellow Juri, radiating joy and optimism.",  );
+    console.log(bouquetArray);
+    storeIntoLocalStorage();
+    i60++;
+  }
+};
+let saveValues70 = document.getElementById('form70');
+saveValues70.addEventListener('submit', handler70)
+function handler70(e) {
+  e.preventDefault();
+  if (i70 == 0) {
+    new bouquet("Birthday", "Light Purble Flower", 5, "Assets/shoppingImages/Shooping7.jpg", " Majestic purple irises evoke a sense of wonder and allure.",  );
+    console.log(bouquetArray);
+    storeIntoLocalStorage();
+    i70++;
+  }
+};
+let saveValues80 = document.getElementById('form80');
+saveValues80.addEventListener('submit', handler80)
+function handler80(e) {
+  e.preventDefault();
+  if (i80 == 0) {
+    new bouquet("Birthday", "Orange Juri Flower", 5, "Assets/shoppingImages/Shooping8.jpg", "Lively orange , bursting with energy and enthusiasm.",  );
+    console.log(bouquetArray);
+    storeIntoLocalStorage();
+    i80++;
+  }
+};
+let saveValues90 = document.getElementById('form90');
+saveValues90.addEventListener('submit', handler90)
+function handler90(e) {
+  e.preventDefault();
+  if (i90 == 0) {
+    new bouquet("Birthday", "Peach Juri Flower", 5, "Assets/shoppingImages/Shooping9.jpg", "Peach flowers have a charming and subtle color that exudes a sense of grace and elegance.",  );
+    console.log(bouquetArray);
+    storeIntoLocalStorage();
+    i90++;
+  }
+};
